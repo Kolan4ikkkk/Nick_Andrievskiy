83677f74ad2e318085df46180aab59f9384f2e31
Kolan4ikkkk <andrievskiy.kolia@gmail.com>
2025-01-17 16:07:50 +0300

    Solve task 5.py

--- exercises/l3_conditions/task5.py
+++ exercises/l3_conditions/task5.py
@@ -1,6 +1,6 @@
 # Write the body of the function to make the script work without errors
 def is_vowel(c: str) -> bool:
-    pass
+     return c.lower() in ['a', 'e', 'i', 'o', 'u']
 
 if __name__ == "__main__":
     # Do not change the below asserts
@@ -8,3 +8,7 @@ if __name__ == "__main__":
     assert is_vowel("e") is True
     assert is_vowel("z") is False
     assert is_vowel("B") is False
+    print(is_vowel("a"))  # True
+    print(is_vowel("e"))  # True
+    print(is_vowel("z"))  # False
+    print(is_vowel("B"))  # False
