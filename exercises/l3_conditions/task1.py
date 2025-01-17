9a03f261cc1062d1c845e94ce675bb750330c536
Kolan4ikkkk <andrievskiy.kolia@gmail.com>
2025-01-17 15:53:52 +0300

    Solve task1.py

--- exercises/l3_conditions/task1.py
+++ exercises/l3_conditions/task1.py
@@ -4,7 +4,7 @@ Number = int | float
 
 # Write the function body to make the script work without errors
 def is_positive(n: Number) -> bool:
-    pass
+    return n >= 0 
 
 
 # Do not change the below's code
@@ -14,3 +14,9 @@ if __name__ == "__main__":
     assert is_positive(3.1) is True
     assert is_positive(-1) is False
     assert is_positive(-0.1) is False
+# Add output of results
+    print(is_positive(3))    # True
+    print(is_positive(0))    # True
+    print(is_positive(3.1))  # True
+    print(is_positive(-1))   # False
+    print(is_positive(-0.1)) # False
