664697d8bcf1b4e16c57b9b463068725308b8f7a
Kolan4ikkkk <andrievskiy.kolia@gmail.com>
2025-01-17 15:32:19 +0300

    Solve task2.py

--- exercises/l2_functions/task2.py
+++ exercises/l2_functions/task2.py
@@ -5,7 +5,9 @@
 # by whitespace ' '.
 #
 # For example, call of `join("a", "b")` should return "a b"
-
+def join(str1: str, str2: str) -> str:
+    print(f"{str1} {str2}")
+    return f"{str1} {str2}"
 
 # Do not change the below's code
 if __name__ == "__main__":
@@ -13,4 +15,4 @@ if __name__ == "__main__":
 
     assert join(a, b) == "Jon Doe"
     assert join(b, a) == "Doe Jon"
-    assert join("aba", "baba") == "aba baba"
+    assert join("Aba", "Baba") == "Aba Baba"
