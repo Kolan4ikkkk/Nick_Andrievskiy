726a5f4d5c11e9a4c13578a3fa36007e33bc7cde
Kolan4ikkkk <andrievskiy.kolia@gmail.com>
2025-01-17 13:46:54 +0300

    Solve task5.py

--- exercises/l1_variables_and_math/task5.py
+++ exercises/l1_variables_and_math/task5.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     b = 3.72
 
     # Modify variables a, b to make the script work without errors
-
+    b = 3.72
+    a = 3.72
+    print(b + a)
     # Do not change the line below
     assert a == b
