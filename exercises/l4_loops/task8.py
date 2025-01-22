5c5c18315586f14857acdcb48b11cdd6c90852c8
Kolan4ikkkk <andrievskiy.kolia@gmail.com>
2025-01-22 13:28:54 +0300

    Solve task8.py

--- exercises/l4_loops/task8.py
+++ exercises/l4_loops/task8.py
@@ -1,7 +1,13 @@
 # Use `for` loop to calculate the amount
 # of positive number in a list of integers `n`
 def count_positive(n: list[int]) -> int:
-    pass
+     count = 0
+     for num in n:
+        if num > 0:
+            count += 1
+            print(count)
+     return count
+
 
 
 # Do not change the below's code
