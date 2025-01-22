1712083ba7991062adc38f74b8b223b5a5dcf35d
Kolan4ikkkk <andrievskiy.kolia@gmail.com>
2025-01-22 13:43:47 +0300

    Solve task10.py

--- exercises/l5_lists/task10.py
+++ exercises/l5_lists/task10.py
@@ -5,7 +5,8 @@ from typing import Any
 # NOTE: the function must create a new list.
 # Do not modify list `l`.
 def reverse(l: list[Any]) -> list[Any]:
-    pass
+    print(l[::-1])
+    return l[::-1]
 
 
 # Do not change the below's code
