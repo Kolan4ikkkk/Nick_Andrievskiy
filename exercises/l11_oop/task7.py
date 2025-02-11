b117b5875fd362d0518770cf2a3e06aec954053a
Kolan4ikkkk <andrievskiy.kolia@gmail.com>
2025-02-11 20:17:37 +0300

    Solve task7.py

--- exercises/l11_oop/task7.py
+++ exercises/l11_oop/task7.py
@@ -7,7 +7,11 @@ class Point:
         self.x = x
         self.y = y
 
-
+    def __add__(self, other):
+        if isinstance(other, Point):
+            print(self.x + other.x, self.y + other.y)
+            return Point(self.x + other.x, self.y + other.y)
+        return NotImplemented
 # Do not change the code below
 if __name__ == "__main__":
     p1 = Point(1, 1)
