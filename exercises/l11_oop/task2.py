bfed5a30b708810c5be771f56cb2146f1ede8260
Kolan4ikkkk <andrievskiy.kolia@gmail.com>
2025-02-11 20:10:16 +0300

    Solve task2.py

--- exercises/l11_oop/task2.py
+++ exercises/l11_oop/task2.py
@@ -1,12 +1,14 @@
 # Add a constructor to the class Vehicle
 class Vehicle:
-    pass
+     def __init__(self, name):
+        self.name = name
 
 
 if __name__ == "__main__":
     # Assign the needed value to the variable `c`
     # to make the script work without errors
-    c = None
+    c = Vehicle("Nissan")
+    print(c.name)
 
     assert isinstance(c, Vehicle)
-    assert c.name == "Mercedes"
+    assert c.name == "Nissan"
